2309b18b0759877943129ffe8c97f1727a041a72
victufell <victor.fellype.a@gmail.com>
2024-12-29 19:17:22 -0400

    feat: adding config

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import AuthProvider from "../(components)/(authProvider)"
+
+const SignIn = () => {
+    return (
+        <AuthProvider>
+            <section>
+                <h1>Form SignIn</h1>
+            </section>
+        </AuthProvider>
+        
+    )
+}
+
+export default SignIn
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,28 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      'orange': {
+        'base':'#F24D0D',
+        'dark': '#C43C08',
       },
+      'blue': {
+        'light': '#D7EFF9',
+        'dark': '#009CF0',
+        'base': '#5EC5FD',
+      },
+      'gray': {
+        100: '#ADADAD',
+        200: '#949494',
+        300: '#666666',
+        400: '#3D3D3D',
+        500: '#1D1D1D'
+      },
+      'white': '#FFFFFF',
+      'background': '#FBF4F4',
+      'shape': '#F5EAEA',
+      'danger': '#DC3545',
+      'success': '#28A745'
     },
   },
   plugins: [],
